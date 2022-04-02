236c43843ff154c28ee19c5ec45362e91904e1a0
sri harsha <kudoark@gmail.com>
2022-04-02 12:49:09 +0530

    fixing spotify

--- enabling_services.sh
+++ enabling_services.sh
@@ -68,13 +68,14 @@ fi
 chmod a+wr /opt/spotify
 chmod a+wr /opt/spotify/Apps -R
 
+cp -r $HOME/.spicetify/Dribbblish $HOME/.config/spicetify/Themes/
 cd "$(dirname "$(spicetify -c)")/Themes/Dribbblish"
 mkdir -p ../../Extensions
 cp dribbblish.js ../../Extensions/.
 spicetify config extensions dribbblish.js
 spicetify config current_theme Dribbblish color_scheme mechanical
 spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
-spicetify apply
+spicetify backup apply
 cd ~
 
 sed -i 's/^#greeter-session=example-gtk-gnome/greeter-session=web-greeter/' /etc/lightdm/lightdm.conf
